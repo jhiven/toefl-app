4fab706f4a1b73bf2807e0a9e3e731ea221620eb
hafidhraffi <raffiadani45@gmail.com>
2024-05-10 10:51:01 +0700

    ui: flipcard

--- lib/test_main.dart
+++ lib/test_main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:toefl_app/domain/state/pick_cubit.dart';
-import 'package:toefl_app/domain/state/synonym_cubit.dart';
-import 'package:toefl_app/presentation/screens/pick_word.dart';
-import 'package:toefl_app/presentation/screens/synonym.dart';
+import 'package:toefl_app/test_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -14,18 +10,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => SynonymCubit(),
-        ),
-        BlocProvider(
-          create: (context) => PickCubit(),
-        )
-      ],
-      child: const MaterialApp(
-        home: PickWord(),
-      ),
+    return MaterialApp(
+      home: TestScreen(),
     );
   }
 }
